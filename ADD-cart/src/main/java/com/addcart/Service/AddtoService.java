1bb5482ff109818f6fb6635b862048e4735bfc6f
nandha1804 <114557417+nandha1804@users.noreply.github.com>
2024-03-11 11:59:07 +0530

    Update AddtoService.java

--- ADD-cart/src/main/java/com/addcart/Service/AddtoService.java
+++ ADD-cart/src/main/java/com/addcart/Service/AddtoService.java
@@ -20,73 +20,73 @@ public class AddtoService {
 
 	public long fun(int pid,int uid,int quantity) {
 		// TODO Auto-generated method stub
-		System.out.println("hiii");
-		String u="http://ADMIN-PRODUCTS/product/updatecheck/"+pid+"/"+quantity;
-	    int s=restTemplate.exchange(u, HttpMethod.PUT,null,int.class).getBody();
+		
+		String url="http://ADMIN-PRODUCTS/product/updatecheck/"+pid+"/"+quantity;
+	    int s=restTemplate.exchange(url, HttpMethod.PUT,null,int.class).getBody();
 	    if(s!=1)
 	    {
 	    	return s;
 	    }
-	    String url="http://ADMIN-PRODUCTS/product/get/"+pid;
+	    String url1="http://ADMIN-PRODUCTS/product/get/"+pid;
 		Item item=restTemplate.getForObject(url,Item.class);
-		Addcart o=new Addcart();
-		o.setPid(pid);
-		o.setUid(uid);
-		o.setImage(item.getImage());
-		o.setOrprice(item.getPrice());
+		Addcart order=new Addcart();
+		order.setPid(pid);
+		oder.setUid(uid);
+		order.setImage(item.getImage());
+		order.setOrprice(item.getPrice());
 	    long price=item.getPrice();
-	    o.setQuantity(quantity);
-	   // o.setSize(size);
-	    o.setPrice(quantity*price);
+	    order.setQuantity(quantity);
+	   // order.setSize(size);
+	    order.setPrice(quantity*price);
 	    System.out.println(quantity*price);
-	    repo.save(o);
+	    repo.save(order);
 		return -1;
 		
 	}
 
 	public long gettotal(int id) {
 		// TODO Auto-generated method stub
-		List<Addcart> ad=repo.getitemsbyuid(id);
-		long tp=0;
-		for(Addcart a:ad)
+		List<Addcart> add=repo.getitemsbyuid(id);
+		long totalPrice=0;
+		for(Addcart a:add)
 		{
-			tp=tp+a.getPrice();
+			totalPrice=totalPrice+a.getPrice();
 		}
-		return tp;
+		return totalPrice;
 	}
 
 	public int getprice(int id) {
 		// TODO Auto-generated method stub
-		List<Addcart> a=repo.findByPid(id);
-		int v=0;
-		for(Addcart c:a)
+		List<Addcart> add=repo.findByPid(id);
+		int price=0;
+		for(Addcart c:add)
 		{
-			v=c.getOrprice();
+			price=c.getOrderprice();
 			break;
 		}
-		return v;
+		return price;
 	}
 	public String updatecartprice(int id,int price) {
 		// TODO Auto-generated method stub
-		List<Addcart> a=repo.findByPid(id);
+		List<Addcart> add=repo.findByPid(id);
 //		String url="http://ADMIN-PRODUCTS/product/get/"+id;
 //		Item item=restTemplate.getForObject(url,Item.class);
 		int v=0,m=0,z=0;
 		
-		for(Addcart c:a)
+		for(Addcart cart:add)
 		{
-			c.setOrprice(price);
-			m=c.getQuantity();
-			c.setPrice(m*price);
-			repo.save(c);
+			cart.setOrprice(price);
+			m=cart.getQuantity();
+			cart.setPrice(m*price);
+			repo.save(cart);
 		}
 		return "updated";
 	}
 
 	public int getproduct(int pid) {
 		// TODO Auto-generated method stub
-		List<Addcart> p= repo.findByPid(pid);
-		System.out.println(p);
+		List<Addcart> product= repo.findByPid(pid);
+		System.out.println(product);
 		/*if(p.size()>0)
 		{
 			return 1;
